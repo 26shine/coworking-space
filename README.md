36aa3a7e26210b1605f02ba438a7177e3725ba41
Shine <26Shiine@gmail.com>
2023-08-16 09:15:32 +0700

    add deployment

--- README.md
+++ README.md
@@ -49,8 +49,7 @@ The database is accessible within the cluster. This means that when you will hav
 
 * Connecting Via Port Forwarding
 ```bash
-kubectl port-forward --namespace default svc/<SERVICE_NAME>-postgresql 5432:5432 &
-    PGPASSWORD="$POSTGRES_PASSWORD" psql --host 127.0.0.1 -U postgres -d postgres -p 5432
+kubectl port-forward --namespace default svc/<SERVICE_NAME>-postgresql 5432:5432 & PGPASSWORD=$POSTGRES_PASSWORD psql --host 127.0.0.1 -U postgres -d postgres -p 5432
 ```
 
 * Connecting Via a Pod
--- db/setup_db.sh
+++ db/setup_db.sh
@@ -3,13 +3,18 @@ echo "Get password for Postgres"
 export POSTGRES_PASSWORD=$(kubectl get secret --namespace default $1-postgresql -o jsonpath="{.data.postgres-password}" | base64 -d)
 
 echo "Starting create schema..." && \
-kubectl port-forward --namespace default svc/$1-postgresql 5432:5432 & PGPASSWORD="$POSTGRES_PASSWORD" psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < 1_create_tables.sql && \
+kubectl port-forward --namespace default svc/$1-postgresql 5432:5432 & PGPASSWORD=$POSTGRES_PASSWORD psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < 1_create_tables.sql && \
 echo "Create schema successfully"
 
-echo "Seeding users..." && \
-kubectl port-forward --namespace default svc/$1-postgresql 5432:5432 & PGPASSWORD="$POSTGRES_PASSWORD" psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < 2_seed_users.sql && \
+echo "Seeding users..." && 
+echo "================================================================" &&\
+kubectl port-forward --namespace default svc/$1-postgresql 5432:5432 & PGPASSWORD=$POSTGRES_PASSWORD psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < 2_seed_users.sql && \
+echo "================================================================" && \
 echo "Seed users successfully"
 
 echo "Seeding tokens..." && \
-kubectl port-forward --namespace default svc/$1-postgresql 5432:5432 & PGPASSWORD="$POSTGRES_PASSWORD" psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < 3_seed_tokens.sql && \
+echo "================================================================" && \
+# Don't need forward again
+psql --host 127.0.0.1 -U postgres -d postgres -p 5432 < 3_seed_tokens.sql && \
+echo "================================================================" && \
 echo "Seed tokens successfully"
